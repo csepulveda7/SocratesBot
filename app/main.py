25b0c8ddf8c50ece2c786d037353d30e1332856e
Sofia Montana <smontana30@live.com>
2020-10-17 21:43:55 -0400

    updates

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import gspread
 from random import randrange
 from tweet import *
@@ -8,10 +9,12 @@ gc = gspread.service_account(filename='gsheet_credentials.json')
 sh = gc.open_by_key('1SbPgsCba39nGz5-vNF6b3q8Qn_12D92TxSizIFtfYmw')
 worksheet = sh.sheet1
 
+
 class Tweet:
         def __init__(self, questions, row_idx):
             self.questions = questions
             self.row_idx = row_idx
+
 
 @app.route('/')
 def tweet_list():
@@ -25,3 +28,14 @@ def tweet_list():
     tweetResponse(tweets[responseID].questions)
     
     return tweets[responseID].questions
+
+
+@app.route('/post', methods = ['GET', 'POST'])
+def postyPost():
+    if request.method == 'POST':
+        print("its a post request")
+        request.headers.add("Access-Control-Allow-Origin", "*")
+        question = request.form.get('question')
+        list = [question]
+        worksheet.append_row(list)
+    print("something")
